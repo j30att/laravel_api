1ca737cf42168c9b3b6d70dfb7643737cf74ebcc
elfill <filonenko0406@gmail.com>
2018-09-21 19:00:17 +0800

    add seeders

--- database/seeds/EventsSeeder.php
+++ database/seeds/EventsSeeder.php
@@ -11,6 +11,39 @@ class EventsSeeder extends Seeder
      */
     public function run()
     {
+        $events = [
+            0 => [
+                'title' => 'super weekend',
+                'image_id' => 1,
+                'fund' => '1000000',
+                'date_start' => '2018-09-21 09:00:00',
+                'date_end' => '2018-10-21 09:00:00'
+            ],
+            1 => [
+                'title' => 'daily majors',
+                'image_id' => 1,
+                'fund' => '500000',
+                'date_start' => '2018-11-21 09:00:00',
+                'date_end' => '2018-12-21 09:00:00'
+            ],
+            2 => [
+                'title' => 'power turbos',
+                'image_id' => 1,
+                'fund' => '100000',
+                'date_start' => '2019-01-21 09:00:00',
+                'date_end' => '2019-03-21 09:00:00'
+            ]
+        ];
+
+        foreach ($events as $event){
+            \App\Models\Event::create([
+                'title' => $event['title'],
+                'image_id' => $event['image_id'],
+                'fund' => $event['fund'],
+                'date_start' => $event['date_start'],
+                'date_end' => $event['date_end'],
+            ]);
+        }
 
     }
 }
---
+++ database/seeds/SubEventsSeeder.php
@@ -0,0 +1,76 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SubEventsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $subEvents = [
+            0 => [
+                'event_id' => '6',
+                'title' => 'saturday',
+                'fund' => '500000',
+                'date_start' => '2018-09-21 09:00:00',
+                'date_end' => '2018-10-22 09:00:00'
+            ],
+            1 => [
+                'event_id' => '6',
+                'title' => 'sunday',
+                'fund' => '500000',
+                'date_start' => '2018-11-23 09:00:00',
+                'date_end' => '2018-12-24 09:00:00'
+            ],
+            2 => [
+                'event_id' => '7',
+                'title' => 'monday',
+                  
+                'fund' => '50000',
+                'date_start' => '2019-01-21 09:00:00',
+                'date_end' => '2019-03-21 09:00:00'
+            ],
+            3 => [
+                'event_id' => '7',
+                'title' => 'tuesday',
+                  
+                'fund' => '60000',
+                'date_start' => '2019-01-21 09:00:00',
+                'date_end' => '2019-03-21 09:00:00'
+            ],
+            4 => [
+                'event_id' => '7',
+                'title' => 'wednesday',
+                  
+                'fund' => '70000',
+                'date_start' => '2019-01-21 09:00:00',
+                'date_end' => '2019-03-21 09:00:00'
+            ],
+            5 => [
+                'event_id' => '8',
+                'title' => 'mega turbo',
+                  
+                'fund' => '7000000',
+                'date_start' => '2019-01-21 09:00:00',
+                'date_end' => '2019-03-21 09:00:00'
+            ],
+
+        ];
+        foreach ($subEvents as $subEvent) {
+            \App\Models\SubEvent::create([
+                'event_id' => $subEvent['event_id'],
+                'title' => $subEvent['title'],
+
+                'fund' => $subEvent['fund'],
+                'date_start' => $subEvent['date_start'],
+                'date_end' => $subEvent['date_end'],
+            ]);
+        }
+
+    }
+}
+
