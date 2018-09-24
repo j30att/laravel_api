27c151173c966c60735399e0cd5be5914617378f
j30att <j30att@gmail.com>
2018-09-24 20:51:34 +0800

    fix views

--- resources/views/events/parts/event.blade.php
+++ resources/views/events/parts/event.blade.php
@@ -1,15 +1,18 @@
 <div class="event-item">
-                    <div class="event-item-wr">
-                        <div class="event-item__img">
-                            <img src="/images/event.png" alt="">
-                        </div>
-                        <div class="event-item__text">
-                            <a href="{{route('event', ['id'=>$event->id])}}">
-                            <div class="event-item__title">{{str_limit($event->title, 20)}}</div>
-                            <div class="event-item__summa">£{{$event->fund}} GTD</div>
-                            </a>
-                            <div class="event-item__date">{{$event->formatted_data}}</div>
-                        </div>
+    <div class="event-item-wr">
+        <a href="{{route('event', ['id'=>$event->id])}}">
+            <div class="event-item__img">
+                <img src="/images/event.png" alt="">
+            </div>
+        </a>
+        <div class="event-item__text">
+            <a href="{{route('event', ['id'=>$event->id])}}">
+                <div class="event-item__title">{{str_limit($event->title, 20)}}</div>
+            </a>
+            <div class="event-item__summa">£{{$event->fund}} GTD</div>
 
-                    </div>
+            <div class="event-item__date">{{$event->formatted_data}}</div>
+        </div>
+
+    </div>
 </div>
