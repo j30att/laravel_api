d5f3ea14fe8355b1f2ab7bea08a0d630c051d900
j30att <j30att@gmail.com>
2018-09-24 20:04:46 +0800

    seeds

--- database/seeds/SubEventsSeeder.php
+++ database/seeds/SubEventsSeeder.php
@@ -13,47 +13,43 @@ class SubEventsSeeder extends Seeder
     {
         $subEvents = [
             0 => [
-                'event_id' => '6',
+                'event_id' => '1',
                 'title' => 'saturday',
                 'fund' => '500000',
                 'date_start' => '2018-09-21 09:00:00',
                 'date_end' => '2018-10-22 09:00:00'
             ],
             1 => [
-                'event_id' => '6',
+                'event_id' => '1',
                 'title' => 'sunday',
                 'fund' => '500000',
                 'date_start' => '2018-11-23 09:00:00',
                 'date_end' => '2018-12-24 09:00:00'
             ],
             2 => [
-                'event_id' => '7',
+                'event_id' => '2',
                 'title' => 'monday',
-                  
                 'fund' => '50000',
                 'date_start' => '2019-01-21 09:00:00',
                 'date_end' => '2019-03-21 09:00:00'
             ],
             3 => [
-                'event_id' => '7',
+                'event_id' => '2',
                 'title' => 'tuesday',
-                  
                 'fund' => '60000',
                 'date_start' => '2019-01-21 09:00:00',
                 'date_end' => '2019-03-21 09:00:00'
             ],
             4 => [
-                'event_id' => '7',
+                'event_id' => '3',
                 'title' => 'wednesday',
-                  
                 'fund' => '70000',
                 'date_start' => '2019-01-21 09:00:00',
                 'date_end' => '2019-03-21 09:00:00'
             ],
             5 => [
-                'event_id' => '8',
+                'event_id' => '3',
                 'title' => 'mega turbo',
-                  
                 'fund' => '7000000',
                 'date_start' => '2019-01-21 09:00:00',
                 'date_end' => '2019-03-21 09:00:00'
