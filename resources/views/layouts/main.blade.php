ca6e5e83564f4270a9cc180f04d3308f50d6a425
elfill <filonenko0406@gmail.com>
2018-09-26 13:26:01 +0800

    add mobile & desktop dir

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,19 +4,15 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('/css/'.$_typeDevice.'/app.css') }}" rel="stylesheet">
         <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
         <title>landing</title>
     </head>
     <body>
         <div class="flex-center position-ref full-height container">
-
-
             <main>
                 @yield('content')
             </main>
-
-
         </div>
     </body>
 </html>
