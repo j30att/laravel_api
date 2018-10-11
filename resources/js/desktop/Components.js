4a3f195ef49d30d38bc6837645af0f69e5f09136
j30att <j30att@gmail.com>
2018-10-11 20:43:49 +0800

    manage sale sidebar

--- resources/js/desktop/Components.js
+++ resources/js/desktop/Components.js
@@ -1,5 +1,6 @@
 import {MainMenuComponent} from "./Components/main-menu/main-menu.controller";
 import {SaleCreateComponent} from "./Components/sale-create/sale-create.controller";
+import {SaleManageComponent} from "./Components/sale-manage/sale-manage.controller";
 import {BidPlaceComponent} from "./Components/bid-place/bid-place.controller"
 
 let angular = require('angular');
@@ -8,6 +9,7 @@ let module = angular.module('Components', []);
 
 module.component('mainMenu', MainMenuComponent);
 module.component('saleCreate', SaleCreateComponent);
+module.component('saleManage', SaleManageComponent);
 module.component('bidPlace', BidPlaceComponent);
 
 export default module.name;
--- resources/js/desktop/Controllers/sales/SaleController.js
+++ resources/js/desktop/Controllers/sales/SaleController.js
@@ -4,11 +4,10 @@ class SaleController {
         this.$scope = $scope;
         this.user = window.__user;
         this._opts = {dataLoad: false, limit:3, openedForm:false};
-
-
         this.SalesResourceService.getMySales(this.user.id, this._opts.limit).then(response =>{
             this.sales = response.data.data;
         });
+
     }
 
     toggleSidenav() {
@@ -16,6 +15,15 @@ class SaleController {
             console.log('open sidenav')
         });
     }
+
+    toggleSidenavManage() {
+        console.log(111111);
+        this.$scope.$broadcast('sidenavManage-open', () =>{
+            console.log('open sidenav')
+        });
+    }
+
+
 
 };
 
