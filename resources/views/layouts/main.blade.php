c2de88e751ab9ea788b3fb59c5d9ab504a51c3ac
j30att <j30att@gmail.com>
2018-10-25 19:00:43 +0800

    static

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0 minimal-ui" />
         <link href="{{ asset('/css/'.$_typeDevice.'/app.css') }}?r={{rand(1000, 10000)}}" rel="stylesheet">
     @if($_platform == 'iOS')
-        <link href="{{ asset('/css/'.$_typeDevice.'/ios/ios.css') }}?r={{rand(1000, 10000)}}" rel="stylesheet">
+        {{--<link href="{{ asset('/css/'.$_typeDevice.'/ios/ios.css') }}?r={{rand(1000, 10000)}}" rel="stylesheet">--}}
     @endif
         <script src="{{ asset('js/'.$_typeDevice.'/app.js') }}?r={{rand(1000, 10000)}}" type="text/javascript"></script>
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
