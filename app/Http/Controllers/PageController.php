4a8b1b27d5ee688c45963ee554996301416194fa
elfill <filonenko0406@gmail.com>
2018-11-13 12:41:42 +0800

    fix

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -10,10 +10,6 @@ use Illuminate\Support\Facades\Log;
 class PageController extends Controller
 {
     public function app(Request $request){
-        $pp = new PPValidate();
-        $pp->authentication();
-
-
         return view('layouts.main');
     }
     public function dealer(Request $request){
