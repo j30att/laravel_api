2e4049b1fa86478750c8226f91a3d39556ea7bc7
j30att <j30att@gmail.com>
2018-11-13 23:29:20 +0800

    update

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -10,7 +10,6 @@ use Illuminate\Support\Facades\Log;
 class PageController extends Controller
 {
     public function app(Request $request){
-        PPInteraction::bidPlace();
         return view('layouts.main');
     }
 
