97f7e9dead20865ae6151099c10656519daee7a4
j30att <j30att@gmail.com>
2018-11-21 20:55:21 +0800

    static

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0 minimal-ui" />
-    <link href="{{ asset('/css/'.$_typeDevice.'/app.css') }}?r={{rand(1000, 10000)}}" rel="stylesheet">
-    <script src="{{ asset('js/'.$_typeDevice.'/app.js') }}?r={{rand(1000, 10000)}}" type="text/javascript"></script>
+    <link href="{{ secure_asset('/css/'.$_typeDevice.'/app.css') }}?r={{rand(1000, 10000)}}" rel="stylesheet">
+    <script src="{{ secure_asset('js/'.$_typeDevice.'/app.js') }}?r={{rand(1000, 10000)}}" type="text/javascript"></script>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
     <title>Poker</title>
     <script>
