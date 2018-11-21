c88ac1924398f9c3390fb69171b51d9eec334863
Dmitry Woron <worond@gmail.com>
2018-11-21 22:31:34 +0800

    Fix uploading avatar and markup on main page

---
+++ config/cloudinary.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'apiKey' => env('CLOUDINARY_API_KEY', ''),
+
+    'apiSecret' => env('CLOUDINARY_API_SECRET', ''),
+
+    'cloudName' => env('CLOUDINARY_CLOUD_NAME', '')
+
+];
