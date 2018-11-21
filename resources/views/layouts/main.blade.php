ebf0c600a8ba4b214880c0517c99dc3742215f86
j30att <j30att@gmail.com>
2018-11-21 19:19:05 +0800

    static

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,13 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    {{--<meta name="viewport" content="width=device-width, initial-scale=1">--}}
+
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0 minimal-ui" />
-    <link href="{{ secure_asset('/css/'.$_typeDevice.'/app.css') }}?r={{rand(1000, 10000)}}" rel="stylesheet">
-    @if($_platform == 'iOS')
-    {{--<link href="{{ secure_asset('/css/'.$_typeDevice.'/ios/ios.css') }}?r={{rand(1000, 10000)}}" rel="stylesheet">--}}
-    @endif
-    <script src="{{ secure_asset('js/'.$_typeDevice.'/app.js') }}?r={{rand(1000, 10000)}}" type="text/javascript"></script>
+    <link href="{{ asset('/css/'.$_typeDevice.'/app.css') }}?r={{rand(1000, 10000)}}" rel="stylesheet">
+    <script src="{{ asset('js/'.$_typeDevice.'/app.js') }}?r={{rand(1000, 10000)}}" type="text/javascript"></script>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
     <title>Poker</title>
     <script>
