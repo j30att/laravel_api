78dfc76d7bc6c04763d6e0a826916811a736ff9c
Dmitry Woron <worond@gmail.com>
2018-11-21 20:43:20 +0800

    Added proxy for api

---
+++ config/api.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'host' => env('PP_API_HOST', ''),
+
+    'partnerName' => env('PP_PARTNER_NAME', ''),
+
+    'authToken' => env('PP_AUTH_TOKEN', ''),
+
+    'useProxy' => env('PP_USE_PROXY', ''),
+
+    'proxyIP' => env('PP_PROXY_IP', '')
+
+];
