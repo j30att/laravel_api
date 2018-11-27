af290691def6af615d1c25bd6ab964946004a4b2
elfill <filonenko0406@gmail.com>
2018-11-27 11:04:20 +0800

    email for mobile

---
+++ resources/views/emails/mobileForgotPassword.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+</head>
+<body>
+    <table border="0" cellpadding="0" cellspacing="0" style="margin:0 auto; padding:0; width: 187px; background: #f6f6f6; font-family: 'Open Sans', sans-serif;">
+        <tr>
+            <td style="text-align: center">
+                <img src="{{\Illuminate\Support\Facades\URL::to('/')}}/images/email/logo.png" alt="logo" style="width: 104px; height: 14.6px; margin: 10px auto">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <table border="0" cellpadding="0" cellspacing="0" style="margin:0 auto; padding:0; width: 172px; background: #ffffff; border-radius: 5px; box-shadow: 0 3px 8px 0 #f2f2f2;">
+                <tr>
+                    <td style="padding-top: 15px; padding-bottom: 5px; font-size: 11px; font-weight: normal; color: #000000; width: 100%; text-align: center;">You’re nearly there...</td>
+                </tr>
+                <tr>
+                    <td style="padding-bottom: 10px; font-size: 8px; font-weight: normal; color: #000000; width: 100%; text-align: center; opacity: 0.5;">To reset your password, please copy this code and paste form in site:</td>
+                </tr>
+                <tr>
+
+                    <td colspan="0">
+                        <a href="" style="text-decoration: none;">
+                            <span style="display:block; width: 80%; margin:auto; height: 22px; line-height:22px; border-radius: 4px; background-color: #007fed; font-size: 5px; text-align: center; color: #ffffff; cursor: pointer">{{$token}}</span>
+                        </a>
+                    </td>
+
+
+
+                </tr>
+                <tr>
+                    <td style="width: 100%; padding: 15px 18px; text-align: center; font-size: 6.5px; color: #949494; line-height: 1.38;">This email is part of our sign-up process. If you didn't want to sign up with MyPartyPokerLive, please ignore it, and after a short period the request will be removed from our system.</td>
+                </tr>
+                <tr>
+                    <td style="width: 100%; height: 1px; background-color: #ededed;"></td>
+                </tr>
+                <tr>
+
+                    <td height="54">
+                        <span style="width: 50%; text-align: center;font-weight: 700; display: block; float: right"><img src="{{\Illuminate\Support\Facades\URL::to('/')}}/images/email/google_play.png" alt="google_play" width="12.8" height="15.2" style="margin-bottom: 2px"><p style="display: block;margin: 0; font-size: 10px">Google Play</p></span>
+                        <span style="width: 50%; text-align: center;font-weight:700; display: block;"><img src="{{\Illuminate\Support\Facades\URL::to('/')}}/images/email/app_store.png" alt="appstore" width="12.5" height="14" style="margin-bottom: 2px"><p style="display: block;margin: 0; font-size: 10px">AppStore</p></span>
+                    </td>
+
+                </tr>
+            </table>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <table border="0" cellpadding="0" cellspacing="0" style="margin:0 auto; padding:0; width: 172.5px; background: #f6f6f6;">
+                    <tr>
+                        <td style="padding-top: 15px; padding-bottom: 11px">
+                            <span style="display: block">
+                                <p style="font-size: 7px; line-height: 1.29; padding-bottom: 8px; width: 100%; text-align: center;">Connect with us on:</p>
+                                <p style="display: block; width: 92px; margin: 0 auto 15px auto">
+                                    <span style="display: block; width: 20px; height: 20px; text-align: center; cursor: pointer; border: 1px solid #e6e6e6;border-radius: 4px; float: left; box-sizing: border-box; padding-top: 6px; margin-right: 4px"><a
+                                            href="#"><img src="{{\Illuminate\Support\Facades\URL::to('/')}}/images/email/twitter.png" alt="twitter" style="width: 9px; height: 7.5px"></a></span>
+                                    <span style="display: block; width: 20px; height: 20px; text-align: center; cursor: pointer; border: 1px solid #e6e6e6;border-radius: 4px; float: left; box-sizing: border-box; padding-top: 5.5px; margin-right: 4px"><a
+                                            href="#"><img src="{{\Illuminate\Support\Facades\URL::to('/')}}/images/email/facebook.png" alt="twitter" style="width: 5px; height: 8.5px"></a></span>
+                                    <span style="display: block; width: 20px; height: 20px; text-align: center; cursor: pointer; border: 1px solid #e6e6e6;border-radius: 4px; float: left; box-sizing: border-box; padding-top: 4.5px; margin-right: 4px"><a
+                                        href="#"><img src="{{\Illuminate\Support\Facades\URL::to('/')}}/images/email/instagram.png" alt="twitter" style="width: 11px; height: 11px"></a></span><span style="display: block; width: 20px; height: 20px; text-align: center; cursor: pointer; border: 1px solid #e6e6e6;border-radius: 4px; float: left; box-sizing: border-box; padding-top: 4.5px"><a
+                                        href="#"><img src="{{\Illuminate\Support\Facades\URL::to('/')}}/images/email/twitter.png" alt="twitter" style="width: 11px; height: 8px"></a></span>
+                                </p>
+
+                            </span>
+                            <br>
+                            <span style="display: block;text-align: center;width: 172.5px; font-size: 6.5px; line-height: 1.38; color: #949494;">You have received this email from MyPartyPokerLive (postal address below). If you do not wish to receive further email communications from MyPartyPokerLive, please click here to opt out. MyPartyPokerLive is a limited company registered in England and Wales under CRN 555777666 with its registered office at 13 Hawley Crescent, London, England, NW1 8NP.</span>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+
+    </table>
+</body>
+</html>
